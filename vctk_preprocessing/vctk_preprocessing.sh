0f028e0135e9ac12a1409b0e9440028264671399
Nikita Usoltsev <usoltsev-nv@yandex.ru>
2022-12-14 15:29:29 +0300

    Add scripts for preprocessing

---
+++ vctk_preprocessing/vctk_preprocessing.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+docker build --rm --tag emotts ./vctk_preprocessing
+
+docker run --rm --shm-size="8G" -it -v $(pwd):/emotts/repo -v /media/public/nikita_u/data/:/emotts/repo/data emotts
